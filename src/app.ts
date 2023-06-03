547fb144d72f3df114ea340e4d8f91f8af76e70e
dev-ashanaur <devashanaur009@gmail.com>
2023-06-04 00:38:39 +0600

    [DONE] create user service,controller ,routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
+//import end point
+import usersRoute from './app/modules/users/users.route'
+
 //use cors
 app.use(cors())
 //parser
@@ -12,5 +15,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req: Request, res: Response) => {
   res.json('Welcome to Auth Service!')
 })
+//application route
+app.use('/api/v1/users', usersRoute)
 
 export default app
