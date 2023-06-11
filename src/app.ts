b4cf36696b829d9ef569731d8bf58388033a87b2
dev-ashanaur <devashanaur009@gmail.com>
2023-06-12 03:54:06 +0600

    [DONE] create global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,22 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-const app: Application = express()
-
-//import end point
-import usersRoute from './app/modules/users/users.route'
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { UserRoute } from './app/modules/user/user.route';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+const app: Application = express();
 
 //use cors
-app.use(cors())
+app.use(cors());
 //parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //welcome route
 app.get('/', (req: Request, res: Response) => {
-  res.json('Welcome to Auth Service!')
-})
+  res.json('Welcome to Auth Service!');
+});
 //application route
-app.use('/api/v1/users', usersRoute)
+app.use('/api/v1/users', UserRoute);
 
-export default app
+app.use(globalErrorHandler);
+
+export default app;
